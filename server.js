9a51f479ad6631ec9c6f3a69c6afbff45167eed9
arsen <arsenavagyanmail@gmail.com>
2018-02-12 15:43:10 +0400

    second commit

---
+++ server.js
@@ -0,0 +1,121 @@
+/*
+  This is the SocketCluster master controller file.
+  It is responsible for bootstrapping the SocketCluster master process.
+  Be careful when modifying the options object below.
+  If you plan to run SCC on Kubernetes or another orchestrator at some point
+  in the future, avoid changing the environment variable names below as
+  each one has a specific meaning within the SC ecosystem.
+*/
+var express = require ('express');
+var mongoose = require('mongoose');
+var app = express();
+mongoose.connect('mongodb://localhost/chat');
+var userSchema = mongoose.Schema({
+    local            : {
+        username        : String,
+        password     : String
+    }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
+
+
+
+var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
+var scHotReboot = require('sc-hot-reboot');
+var scErrors = require('sc-errors');
+var TimeoutError = scErrors.TimeoutError;
+
+var fsUtil = require('socketcluster/fsutil');
+var waitForFile = fsUtil.waitForFile;
+
+var SocketCluster = require('socketcluster');
+
+var workerControllerPath = argv.wc || process.env.SOCKETCLUSTER_WORKER_CONTROLLER;
+var brokerControllerPath = argv.bc || process.env.SOCKETCLUSTER_BROKER_CONTROLLER;
+var workerClusterControllerPath = argv.wcc || process.env.SOCKETCLUSTER_WORKERCLUSTER_CONTROLLER;
+var environment = process.env.ENV || 'dev';
+var os = require('os').cpus().length;
+var options = {
+  workers:  os,
+  brokers: Number(argv.b) || Number(process.env.SOCKETCLUSTER_BROKERS) || 1,
+  port: Number(argv.p) || Number(process.env.SOCKETCLUSTER_PORT) || 3000,
+  // If your system doesn't support 'uws', you can switch to 'ws' (which is slower but works on older systems).
+  wsEngine: process.env.SOCKETCLUSTER_WS_ENGINE || 'uws',
+  appName: argv.n || process.env.SOCKETCLUSTER_APP_NAME || null,
+  workerController: workerControllerPath || __dirname + '/worker.js',
+  brokerController: brokerControllerPath || __dirname + '/broker.js',
+  workerClusterController: workerClusterControllerPath || null,
+  socketChannelLimit: Number(process.env.SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT) || 1000,
+  clusterStateServerHost: argv.cssh || process.env.SCC_STATE_SERVER_HOST || null,
+  clusterStateServerPort: process.env.SCC_STATE_SERVER_PORT || null,
+  clusterAuthKey: process.env.SCC_AUTH_KEY || null,
+  clusterInstanceIp: process.env.SCC_INSTANCE_IP || null,
+  clusterInstanceIpFamily: process.env.SCC_INSTANCE_IP_FAMILY || null,
+  clusterStateServerConnectTimeout: Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || null,
+  clusterStateServerAckTimeout: Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || null,
+  clusterStateServerReconnectRandomness: Number(process.env.SCC_STATE_SERVER_RECONNECT_RANDOMNESS) || null,
+  crashWorkerOnError: argv['auto-reboot'] != false,
+  // If using nodemon, set this to true, and make sure that environment is 'dev'.
+  killMasterOnSignal: false,
+  environment: environment
+};
+
+var bootTimeout = Number(process.env.SOCKETCLUSTER_CONTROLLER_BOOT_TIMEOUT) || 10000;
+var SOCKETCLUSTER_OPTIONS;
+
+if (process.env.SOCKETCLUSTER_OPTIONS) {
+  SOCKETCLUSTER_OPTIONS = JSON.parse(process.env.SOCKETCLUSTER_OPTIONS);
+}
+
+for (var i in SOCKETCLUSTER_OPTIONS) {
+  if (SOCKETCLUSTER_OPTIONS.hasOwnProperty(i)) {
+    options[i] = SOCKETCLUSTER_OPTIONS[i];
+  }
+}
+
+var start = function () {
+  var socketCluster = new SocketCluster(options);
+
+  socketCluster.on(socketCluster.EVENT_WORKER_CLUSTER_START, function (workerClusterInfo) {
+    console.log('   >> WorkerCluster PID:', workerClusterInfo.pid);
+  });
+
+  if (socketCluster.options.environment == 'dev') {
+    // This will cause SC workers to reboot when code changes anywhere in the app directory.
+    // The second options argument here is passed directly to chokidar.
+    // See https://github.com/paulmillr/chokidar#api for details.
+    console.log(`   !! The sc-hot-reboot plugin is watching for code changes in the ${__dirname} directory`);
+    scHotReboot.attach(socketCluster, {
+      cwd: __dirname,
+      ignored: ['public', 'node_modules', 'README.md', 'Dockerfile', 'server.js', 'broker.js', /[\/\\]\./, '*.log']
+    });
+  }
+};
+
+var bootCheckInterval = Number(process.env.SOCKETCLUSTER_BOOT_CHECK_INTERVAL) || 200;
+var bootStartTime = Date.now();
+
+// Detect when Docker volumes are ready.
+var startWhenFileIsReady = (filePath) => {
+  var errorMessage = `Failed to locate a controller file at path ${filePath} ` +
+  `before SOCKETCLUSTER_CONTROLLER_BOOT_TIMEOUT`;
+
+  return waitForFile(filePath, bootCheckInterval, bootStartTime, bootTimeout, errorMessage);
+};
+
+var filesReadyPromises = [
+  startWhenFileIsReady(workerControllerPath),
+  startWhenFileIsReady(brokerControllerPath),
+  startWhenFileIsReady(workerClusterControllerPath)
+];
+Promise.all(filesReadyPromises)
+.then(() => {
+  start();
+})
+.catch((err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
---
+++ worker.js
@@ -0,0 +1,75 @@
+var SCWorker = require('socketcluster/scworker');
+var fs = require('fs');
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var morgan = require('morgan');
+var healthChecker = require('sc-framework-health-check');
+
+class Worker extends SCWorker {
+    run() {
+        console.log('   >> Worker PID:', process.pid);
+        var environment = this.options.environment;
+
+        var app = express();
+
+        var httpServer = this.httpServer;
+        var scServer = this.scServer;
+
+        if (environment == 'dev') {
+            // Log every HTTP request. See https://github.com/expressjs/morgan for other
+            // available formats.
+            app.use(morgan('dev'));
+        }
+        app.use(serveStatic(path.resolve(__dirname, 'public')));
+
+        // Add GET /health-check express route
+        healthChecker.attach(this, app);
+
+        httpServer.on('request', app);
+
+        var count = 0;
+
+        /*
+          In here we handle our incoming realtime connections and listen for events.
+        */
+        scServer.on('connection', function (socket) {
+            socket.on('connect' , () => {
+                console.log('user connect');
+            });
+            socket.on('login', function (data,respond) {
+                if (data === 'sa') {
+                    respond();
+                    socket.setAuthToken({username: data});
+                    console.log('you loggin succssesfully')
+
+                } else {
+                    console.log('fail login')
+
+                }
+
+                scServer.addMiddleware(scServer.MIDDLEWARE_PUBLISH_IN, function (req, next) {
+                    var authToken = req.socket.authToken;
+                    if (authToken) {
+                        console.log(authToken)
+                        next();
+                    } else {
+                        next('You are not authorized to publish to ' + req.channel);
+                    }
+                });
+
+
+
+            });
+            socket.on('chat', function (data) {
+                scServer.exchange.publish('startchat', data)
+            });
+
+
+
+        });
+
+    }
+}
+
+new Worker();
